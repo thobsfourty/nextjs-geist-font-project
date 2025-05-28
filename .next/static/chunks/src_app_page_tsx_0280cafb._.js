b1b94d262f58ddd32391f28a6a9a83c0b7f66fb0
thobsfourty <thobs40@outlook.com>
2025-05-28 23:46:08 +0200

    Add .next/static/chunks/src_app_page_tsx_0280cafb._.js 1748468767552

---
+++ .next/static/chunks/src_app_page_tsx_0280cafb._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_99ed1588._.js",
+  "static/chunks/src_components_71e7892e._.js"
+],
+    source: "dynamic"
+});
